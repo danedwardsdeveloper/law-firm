40b554913eb947ed9cf5241c7a8742ef0181637d
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-29 21:11:42 +0000

    Add styles

--- src/app/team/TeamMemberCard.tsx
+++ src/app/team/TeamMemberCard.tsx
@@ -7,7 +7,14 @@ export default function TeamMemberCard({ teamMember, priority }: { teamMember: T
 		<Link href={`/team/${teamMember.slug}`}>
 			<h2 className="text-xl font-medium">{teamMember.title}</h2>
 			<p className="mb-2">{teamMember.role}</p>
-			<Image src={teamMember.featuredImage} alt={teamMember.metaDescription} width={1200} height={630} priority={priority} />
+			<Image
+				src={teamMember.featuredImage}
+				alt={teamMember.metaDescription}
+				width={1200}
+				height={630}
+				priority={priority}
+				className="rounded-md"
+			/>
 		</Link>
 	)
 }
