2e5a37ed7c294d1fa078149a4716605306ee7551
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-29 21:10:57 +0000

    Add font, remove Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
-import Providers from '@/components/Providers'
 import { dynamicBaseURL } from '@/library/environment/publicVariables'
 import type { Metadata, Viewport } from 'next'
-
 import './styles.tailwind.css'
 import Menu from '@/components/Menu'
+import { Plus_Jakarta_Sans } from 'next/font/google'
 import type { ReactNode } from 'react'
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-plus-jakarta-sans',
+})
 
 export const metadata: Metadata = {
 	title: 'Archer Finch Legal',
@@ -29,12 +34,10 @@ export default function RootLayout({
 	children: ReactNode
 }>) {
 	return (
-		<html lang="en-GB" suppressHydrationWarning>
-			<body className="flex flex-col w-full min-h-screen">
-				<Providers>
-					<Menu />
-					<div className="max-w-4xl w-full mx-auto mt-12 mb-20">{children}</div>
-				</Providers>
+		<html lang="en-GB" suppressHydrationWarning className={plusJakartaSans.className}>
+			<body className="flex flex-col w-full min-h-screen bg-amber-50">
+				<Menu />
+				<div className="max-w-4xl w-full mx-auto mt-12 mb-20 px-4 lg:px-0">{children}</div>
 			</body>
 		</html>
 	)
