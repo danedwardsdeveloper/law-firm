65752fada4b3439a779c28e0b6be046166d51ea0
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-30 15:01:22 +0100

    Delete unused template content

---
+++ src/library/constants/definitions/metadataPhrases.ts
@@ -0,0 +1,7 @@
+export const titleMetadataPhrases = [
+	'Archer Finch Legal, Intellectual property lawyers',
+	'Archer Finch Legal, Intellectual property law',
+	'Archer Finch Legal, Intellectual property',
+	'Archer Finch Legal, IP lawyers',
+	'Archer Finch Legal, London',
+]
--- src/library/utilities/browser/index.ts
+++ src/library/utilities/browser/index.ts
@@ -2,4 +2,3 @@ import { oxford as formatList } from 'humanize-plus'
 import { cn as mergeClasses } from './definitions/shadcn'
 
 export { formatList, mergeClasses }
-export * from './definitions/createNewDate'
