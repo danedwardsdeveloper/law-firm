0f7191c51d3d1bd693e0126d0396d0524fa6b261
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-31 12:09:30 +0100

    Add Hero and Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import Hero from '@/components/Hero'
+import Testimonials from '@/components/Testimonials'
 import { getTeamMembers } from '@/library/wordpress/getTeamMembers'
 import TeamMemberCard from './team/TeamMemberCard'
+import { testimonials } from './testimonials/data'
 
 export default async function HomePage() {
 	const allTeamMembers = await getTeamMembers()
@@ -7,10 +10,15 @@ export default async function HomePage() {
 
 	return (
 		<>
-			<section className="mb-20">
-				<h1>Home</h1>
+			<section className="md:mt-20 mb-40">
+				<Hero />
 			</section>
-			<section className="">
+			<section className="mb-20">
+				<h2 className="text-xl font-medium mb-4">Testimonials</h2>
+				<p className="mb-10">What people have to say about our award-winning IP law firm</p>
+				<Testimonials testimonials={testimonials.slice(0, 2)} />
+			</section>
+			<section className="mb-20">
 				<h2 className="text-2xl font-medium mb-4">Team</h2>
 				<p className="mb-12">Learn more about our team</p>
 				<div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-16">
