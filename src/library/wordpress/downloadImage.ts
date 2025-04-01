b66cbca7a42ebf3b5570e3df76190f5056adb60b
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-01 13:26:33 +0100

    Don't save to src/app/

--- src/library/wordpress/downloadImage.ts
+++ src/library/wordpress/downloadImage.ts
@@ -1,28 +1,19 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import axios from 'axios'
-import { oxford } from 'humanize-plus'
 import urlJoin from 'proper-url-join'
-import { wordpressMedia } from '../environment/publicVariables'
-import logger from '../logger'
-import { copyImageToApp } from './copyImageToApp'
+import { wordpressMedia } from '../../environment/publicVariables'
+import logger from '../../logger'
 
-export async function downloadImage({
-	imageFileName,
-	subFolder,
-	saveToPublic = false,
-	saveToApp = true,
-}: { imageFileName: string; subFolder: string; saveToPublic: boolean; saveToApp: boolean }) {
-	const publicDir = path.join(process.cwd(), 'public', 'images', 'wordpress', subFolder)
-	const appDir = path.join(process.cwd(), 'src', 'app', 'images', 'wordpress', subFolder)
+export async function downloadImage({ imageFileName, subFolder }: { imageFileName: string; subFolder: string }) {
+	const relativeFolderPath = path.join('public', 'images', 'wordpress', subFolder)
+	const publicDir = path.join(process.cwd(), relativeFolderPath)
 
 	const publicPath = path.join(publicDir, imageFileName)
-	const appPath = path.join(appDir, imageFileName)
 
-	const publicExists = saveToPublic && fs.existsSync(publicPath)
-	const appExists = saveToApp && fs.existsSync(appPath)
+	const publicExists = fs.existsSync(publicPath)
 
-	if ((saveToPublic && publicExists) || (saveToApp && appExists)) return
+	if (publicExists) return
 
 	try {
 		const response = await axios({
@@ -33,26 +24,15 @@ export async function downloadImage({
 
 		const imageBuffer = Buffer.from(response.data, 'binary')
 
-		// First write the file to public
 		if (!fs.existsSync(publicDir)) {
 			fs.mkdirSync(publicDir, { recursive: true })
 		}
 
 		fs.writeFileSync(publicPath, imageBuffer)
 
-		// Then copy over to app if needed
-		// Next.js doesn't allow you to write directly inside /src/app so this is a workaround
-		if (saveToApp) copyImageToApp(imageFileName, subFolder)
+		const relativeFilePath = path.join(relativeFolderPath, imageFileName)
 
-		// Then delete the public file if not needed
-		if (!saveToPublic) {
-			try {
-				fs.unlinkSync(publicPath)
-			} catch (deleteError) {
-				logger.warn(`Failed to delete temporary file from public: ${publicPath}`, deleteError)
-			}
-		}
-		logger.success(`Downloaded ${imageFileName} to ${oxford([saveToPublic ? 'public' : null, saveToApp ? 'app' : null].filter(Boolean))}`)
+		logger.success(`Downloaded ${imageFileName}: ${relativeFilePath}`)
 	} catch (error) {
 		logger.error(`Error downloading image ${imageFileName}:`, error)
 		throw error
