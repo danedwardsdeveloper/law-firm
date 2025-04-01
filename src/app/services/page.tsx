42669c7c0d053dfb92915cee4677d7af9d51180e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-01 13:31:32 +0100

    Remove broken WordPress function

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,8 +1,8 @@
 import ServiceCard from '@/components/ServiceCard'
-import { getServices } from '@/library/wordpress/getServices'
 
 export default async function ServicesPage() {
 	const allServices = await getServices()
+
 	return (
 		<>
 			<h1 className="text-4xl font-bold mb-12">Services</h1>
