f57e0f83cb4722396e42a499aa7524108edbe484
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-01 13:25:52 +0100

    Destructure service props

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,11 +1,14 @@
 import type { Service } from '@/types'
 import Image from 'next/image'
+import Link from 'next/link'
 
-export default function ServiceCard({ service, priority = false }: { service: Service; priority?: boolean }) {
+export default function ServiceCard({ service: { title, photo, slug }, priority = false }: { service: Service; priority?: boolean }) {
 	return (
-		<li className="flex flex-col gap-y-2">
-			<div className="">{service.title}</div>
-			<Image src={service.photo} alt="" height={1920} width={1281} priority={priority} className="w-full max-w-md" />
+		<li>
+			<Link href={`/services/${slug}`} className="flex flex-col gap-y-2 hover:opacity-80 transition-opacity duration-300">
+				<h2 className="text-2xl font-medium">{title}</h2>
+				<Image src={photo} alt="" height={1920} width={1281} priority={priority} className="w-full max-w-md" />
+			</Link>
 		</li>
 	)
 }
