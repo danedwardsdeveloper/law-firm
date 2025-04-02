ed2db38544261351dc5ba070f9e3762af367f494
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-02 10:13:22 +0100

    Use Payload instead of WordPress

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -2,12 +2,15 @@ import type { Service } from '@/types'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function ServiceCard({ service: { title, photo, slug }, priority = false }: { service: Service; priority?: boolean }) {
+export default function ServiceCard({
+	service: { serviceType, slug, featuredImage },
+	priority = false,
+}: { service: Service; priority?: boolean }) {
 	return (
 		<li>
 			<Link href={`/services/${slug}`} className="flex flex-col gap-y-2 hover:opacity-80 transition-opacity duration-300">
-				<h2 className="text-2xl font-medium">{title}</h2>
-				<Image src={photo} alt="" height={1920} width={1281} priority={priority} className="w-full max-w-md" />
+				<h2 className="text-2xl font-medium">{serviceType}</h2>
+				<Image src={featuredImage.url} alt="" height={1920} width={1281} priority={priority} className="w-full max-w-md" />
 			</Link>
 		</li>
 	)
