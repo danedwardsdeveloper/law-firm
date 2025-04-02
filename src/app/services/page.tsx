0775ca57c1f303cd386a2d1d036d7b7328cc5eb1
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-02 10:15:55 +0100

    Update undefined getServices and map key

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,14 +1,18 @@
 import ServiceCard from '@/components/ServiceCard'
+import { getServices } from '@/library/cms/payload/getServices'
+import { notFound } from 'next/navigation'
 
 export default async function ServicesPage() {
 	const allServices = await getServices()
+
+	if (!allServices) return notFound()
 
 	return (
 		<>
 			<h1 className="text-4xl font-bold mb-12">Services</h1>
 			<ul className="flex flex-col gap-y-16">
 				{allServices.map((service, index) => (
-					<ServiceCard key={index} service={service} priority={index < 2} />
+					<ServiceCard key={service.id} service={service} priority={index < 2} />
 				))}
 			</ul>
 		</>
