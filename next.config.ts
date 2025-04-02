bdc2a884a5e0b1e04797007cf11e0fcfad0bf848
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-02 10:14:25 +0100

    Allow generic localhost

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: 'http',
-				hostname: 'localhost',
-				port: '8000',
-				pathname: '/wp-content/uploads/**',
-			},
-		],
+		remotePatterns: [{ hostname: 'localhost' }],
 	},
 	async headers() {
 		return [
