1538204a9e7505fa01ef261557359e4ca81a5aab
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-05 11:17:52 +0100

    Add email variables

--- src/library/environment/serverVariables.ts
+++ src/library/environment/serverVariables.ts
@@ -1,3 +1,7 @@
 import { requireVariable } from './requireVariable'
 
 export const payloadCmsSecret = requireVariable('PAYLOAD_CMS_SECRET')
+
+export const awsAccessKeyId = requireVariable('AWS_ACCESS_KEY_ID')
+export const awsSecretAccessKey = requireVariable('AWS_SECRET_ACCESS_KEY')
+export const myPersonalEmail = requireVariable('MY_PERSONAL_EMAIL')
