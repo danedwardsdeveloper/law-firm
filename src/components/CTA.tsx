7ee83bf1e862c2e70d2df81e8ef76d92493d6d3b
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-10 21:31:36 +0100

    Add additional optional onClick

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -2,12 +2,22 @@
 import { mergeClasses } from '@/library/utilities/browser'
 import { useProvider } from './Provider'
 
-export default function CTA({ classes }: { classes?: string }) {
+/**
+ * Opens the contact form modal
+ * Accepts an optional onClick property so that the CTA on the mobile menu can close the mobile menu panel at the same time, otherwise scrolling is disabled and the form isn't useable
+ */
+export default function CTA({ classes, onClick }: { classes?: string; onClick?: () => void }) {
 	const { setContactFormVisible } = useProvider()
+
+	const handleClick = () => {
+		setContactFormVisible(true)
+		if (onClick) onClick()
+	}
+
 	return (
 		<button
 			type="button"
-			onClick={() => setContactFormVisible(true)}
+			onClick={handleClick}
 			className={mergeClasses(
 				'bg-green-600 hover:bg-green-500 active:bg-green-400 sm:active:bg-green-500 hover:shadow-md active:shadow-md  transition-all duration-300 px-4 py-1 rounded-lg font-bold text-xl text-white',
 				classes,
