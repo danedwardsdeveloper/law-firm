9c22ce5ca7fd507726bc198b19447755ff5c4c28
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-10 10:31:47 +0100

    Add BreadCrumbs and main content area

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,17 +1,25 @@
-import { getArticles } from '@/library/cms/wordpress'
+import BreadCrumbs from '@/components/BreadCrumbs'
+import { getPayloadArticles } from '@/library/cms/payload/getArticles'
 import ArticleCard from './ArticleCard'
 
 export default async function ArticlesPage() {
-	const allArticles = await getArticles()
+	const allArticles = await getPayloadArticles()
 
 	return (
 		<>
-			<h1>Articles</h1>
-			<div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-				{allArticles.map((article, index) => (
-					<ArticleCard key={article.slug} article={article} priority={index < 2} />
-				))}
-			</div>
+			<BreadCrumbs current="Articles" />
+			<main id="main-content">
+				<h1 className="text-4xl font-bold mb-8 text-zinc-900">Articles</h1>
+				<p className="mb-16 max-w-2xl text-lg text-pretty">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+					enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</p>
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-24">
+					{allArticles.map((article, index) => (
+						<ArticleCard key={article.slug} article={article} priority={index < 2} />
+					))}
+				</div>
+			</main>
 		</>
 	)
 }
