a478f9108e9835c07494a415e56874e3d11823f2
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-10 10:38:21 +0100

    Add tagline

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -3,15 +3,23 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function ServiceCard({
-	service: { serviceType, slug, featuredImage },
+	service: { serviceType, tagline, slug, featuredImage },
 	priority = false,
 }: { service: Service; priority?: boolean }) {
 	return (
-		<li>
-			<Link href={`/services/${slug}`} className="flex flex-col gap-y-2 hover:opacity-80 transition-opacity duration-300">
-				<h2 className="text-2xl font-medium">{serviceType}</h2>
-				<Image src={featuredImage.url} alt="" height={1200} width={630} priority={priority} className="w-full max-w-md" />
-			</Link>
-		</li>
+		<Link href={`/services/${slug}`} className="flex flex-col gap-y-2 hover:opacity-80 transition-opacity duration-300">
+			<Image
+				src={featuredImage.url}
+				alt=""
+				height={1200}
+				width={630}
+				priority={priority}
+				// ToDo: placeholder handling
+				// placeholder="blur"
+				className="w-full max-w-md mb-2"
+			/>
+			<h2 className="text-2xl font-medium mb-1">{serviceType}</h2>
+			<p>{tagline}</p>
+		</Link>
 	)
 }
