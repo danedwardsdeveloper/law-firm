c34e6abd22d1a85b88fd33bd44ebf167fb1d856f
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-10 10:35:09 +0100

    Add BreadCrumbs and main-content link target

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -1,19 +1,21 @@
+import BreadCrumbs from '@/components/BreadCrumbs'
 import Testimonials from '@/components/Testimonials'
 import { testimonials } from './data'
 
 export default function TestimonialsPage() {
 	return (
 		<>
-			<header className="mb-20">
-				<h1>Testimonials</h1>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-					enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</p>
-			</header>
-			<section className="mb-20">
+			<BreadCrumbs current="Testimonials" />
+			<main id="main-content">
+				<div className="mb-20">
+					<h1 className="text-4xl font-bold mb-12 text-zinc-900">Testimonials</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+						enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					</p>
+				</div>
 				<Testimonials testimonials={testimonials} priorityIndices={[0]} />
-			</section>
+			</main>
 		</>
 	)
 }
