ca7be2c1de0aab5af08754f3e9cfce3697491d7f
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-10 10:38:37 +0100

    Create getArticles.ts

---
+++ src/library/cms/payload/getArticles.ts
@@ -0,0 +1,91 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { payloadCmsBase } from '@/library/environment/publicVariables'
+import { payloadCmsSecret } from '@/library/environment/serverVariables'
+import logger from '@/library/logger'
+import { downloadImage } from '@/library/utilities/server'
+import type { PayloadArticle } from '@/types'
+import { getPlaiceholder } from 'plaiceholder'
+import urlJoin from 'proper-url-join'
+import { sortArticles } from './sortArticles'
+
+let articlesCache: PayloadArticle[] | undefined = undefined
+
+export async function getPayloadArticles(): Promise<PayloadArticle[]> {
+	if (articlesCache) return articlesCache
+
+	try {
+		const cacheBust = `?t=${Date.now()}`
+		const response = await fetch(urlJoin(payloadCmsBase, 'api/articles', cacheBust), {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${payloadCmsSecret}`,
+			},
+		})
+
+		if (!response.ok) {
+			logger.warn('Fetch Payload articles response not okay. Is the Payload server running?')
+			return []
+		}
+
+		const data = await response.json()
+
+		const processedArticles: PayloadArticle[] = []
+
+		for (const document of data.docs) {
+			try {
+				const imageFileName = document.featuredImage.url.split('/').pop()
+
+				const payloadFolder = '/images/payload'
+				const subFolder = 'articles/featured'
+
+				await downloadImage({
+					imageFileName,
+					subFolder,
+					cms: 'payload',
+				})
+
+				const downloadedFilePath = path.join(process.cwd(), 'public', payloadFolder, subFolder, imageFileName)
+				const downloadedFile = fs.readFileSync(downloadedFilePath)
+
+				const { base64 } = await getPlaiceholder(downloadedFile)
+
+				const processedArticle: PayloadArticle = {
+					...document,
+					featuredImagePlaceholder: base64,
+					featuredImage: {
+						alt: document.featuredImage.alt,
+						url: urlJoin(payloadFolder, subFolder, imageFileName, {
+							leadingSlash: true,
+						}),
+					},
+				}
+
+				processedArticles.push(processedArticle)
+			} catch (error) {
+				logger.error(`Error processing article "${document.serviceType || 'unknown'}":`, error)
+			}
+		}
+
+		if (processedArticles.length === 0) {
+			logger.warn('No valid articles found')
+			return []
+		}
+
+		const sortedArticles = sortArticles(processedArticles)
+
+		articlesCache = sortedArticles
+
+		return articlesCache
+	} catch (error) {
+		const errorMessage = `getPayloadArticles error: ${error instanceof Error ? error.message : String(error)}`
+		logger.warn(errorMessage)
+		return []
+	}
+}
+
+export async function getPayloadArticleBySlug(slug: string): Promise<PayloadArticle | undefined> {
+	const articles = await getPayloadArticles()
+	const article = articles.find((article) => article.slug === slug)
+	return article || undefined
+}
