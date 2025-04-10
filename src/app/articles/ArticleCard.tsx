448b202f44aae4565708e7ea8e463d07958d5c76
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-10 10:31:23 +0100

    Update ArticleCard.tsx

--- src/app/articles/ArticleCard.tsx
+++ src/app/articles/ArticleCard.tsx
@@ -1,13 +1,31 @@
-import type { Article } from '@/types'
+import type { PayloadArticle } from '@/types'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function ArticleCard({ article, priority }: { article: Article; priority: boolean }) {
+export default function ArticleCard({
+	article: {
+		slug,
+		featuredImagePlaceholder,
+		excerpt,
+		title,
+		featuredImage: { url, alt },
+	},
+	priority,
+}: { article: PayloadArticle; priority: boolean }) {
 	return (
-		<Link href={`/articles/${article.slug}`}>
-			<h2 className="text-xl font-medium">{article.title}</h2>
-			<p className="mb-2">{article.categories[0]}</p>
-			{article.featuredImage && <Image src={article.featuredImage} alt={article.excerpt} width={1200} height={630} priority={priority} />}
+		<Link href={`/articles/${slug}`}>
+			<Image
+				src={url}
+				alt={alt}
+				width={1200}
+				height={630}
+				priority={priority}
+				placeholder="blur"
+				blurDataURL={featuredImagePlaceholder}
+				className="rounded-md mb-2 w-full"
+			/>
+			<h2 className="text-xl font-medium mb-1">{title}</h2>
+			<p>{excerpt}</p>
 		</Link>
 	)
 }
