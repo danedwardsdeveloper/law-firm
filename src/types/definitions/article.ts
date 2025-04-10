ac3aaaf039a9c4caa15e0be7b9109be2141922f6
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-10 10:41:15 +0100

    Replace Article with PayloadArticle

--- src/types/definitions/article.ts
+++ src/types/definitions/article.ts
@@ -1,10 +1,31 @@
-export type Article = {
-	id: string
+import type { PayloadMedia } from './payload'
+
+export interface PayloadArticle {
+	id: number
 	title: string
 	slug: string
+	metatitle: string
 	excerpt: string
-	content: string
-	date: string
-	featuredImage?: string
-	categories: number[]
+	metadescription: string
+	featuredImage: PayloadMedia
+	featuredImagePlaceholder: string
+	policyPage?: boolean | null
+	companyPage?: boolean | null
+	content: {
+		root: {
+			type: string
+			children: {
+				type: string
+				version: number
+				[k: string]: unknown
+			}[]
+			direction: ('ltr' | 'rtl') | null
+			format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | ''
+			indent: number
+			version: number
+		}
+		[k: string]: unknown
+	}
+	updatedAt: string
+	createdAt: string
 }
