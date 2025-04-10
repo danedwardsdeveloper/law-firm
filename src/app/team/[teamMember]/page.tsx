40df83979facd3e04e357e3a38fba336f6d2f52d
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-10 10:34:01 +0100

    Add BreadCrumbs and main-content link target

--- src/app/team/[teamMember]/page.tsx
+++ src/app/team/[teamMember]/page.tsx
@@ -1,4 +1,5 @@
-import { getTeamMemberBySlug, getTeamMembers } from '@/library/cms/wordpress'
+import BreadCrumbs from '@/components/BreadCrumbs'
+import { getTeamMemberBySlug, getTeamMembers } from '@/library/cms/wordpress/getTeamMembers'
 import type { Metadata } from 'next'
 import Image from 'next/image'
 import { notFound } from 'next/navigation'
@@ -44,23 +45,27 @@ export default async function TeamMemberPage({ params }: { params: Params }) {
 	}
 
 	return (
-		<div className="flex flex-col gap-y-4">
-			<p className="font-medium">{teamMember.role}</p>
-			<h1>{teamMember.title}</h1>
-			<Image
-				src={teamMember.featuredImage}
-				alt={teamMember.metaDescription}
-				width={1200}
-				height={630}
-				priority
-				sizes="421"
-				className="w-full max-w-xl rounded-md mb-12"
-			/>
-			<div
-				className="flex flex-col gap-y-4 leading-7 text-zinc-700 max-w-prose text-lg"
-				// biome-ignore lint/security/noDangerouslySetInnerHtml:
-				dangerouslySetInnerHTML={{ __html: teamMember.content }}
-			/>
-		</div>
+		<>
+			<BreadCrumbs trail={[{ display: 'Team', href: '/team' }]} current={teamMember.title} />
+			<main className="flex flex-col gap-y-4" id="main-content">
+				<p className="font-medium">{teamMember.role}</p>
+				<h1>{teamMember.title}</h1>
+				<Image
+					src={teamMember.featuredImage}
+					alt={teamMember.metaDescription}
+					width={1200}
+					height={630}
+					priority
+					sizes="421"
+					placeholder="blur"
+					className="w-full max-w-xl rounded-md mb-12"
+				/>
+				<div
+					className="flex flex-col gap-y-8 leading-8 text-zinc-700 max-w-prose text-lg"
+					// biome-ignore lint/security/noDangerouslySetInnerHtml:
+					dangerouslySetInnerHTML={{ __html: teamMember.content }}
+				/>
+			</main>
+		</>
 	)
 }
