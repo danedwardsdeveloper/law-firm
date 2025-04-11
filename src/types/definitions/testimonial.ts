1f51697134767c1e4bbbd58d93fcf8cfd9c1ed00
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-11 13:29:07 +0100

    Add index

--- src/types/definitions/testimonial.ts
+++ src/types/definitions/testimonial.ts
@@ -2,6 +2,7 @@ import type { StaticImageData } from 'next/image'
 import type { ServiceName } from './services'
 
 export interface Testimonial {
+	index: number
 	service: ServiceName
 	writer: {
 		name: string
