36e01da2088d0e088f3054f674c5d47b709ce56f
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-12 10:20:48 +0100

    Create CtaSection.tsx

---
+++ src/components/CtaSection.tsx
@@ -0,0 +1,19 @@
+import CTA from './CTA'
+
+export default function CtaSection() {
+	return (
+		<div className="bg-green-600">
+			<div className="mx-auto max-w-4xl px-6 lg:px-0 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between">
+				<h2 className="max-w-2xl text-4xl font-semibold tracking-tight text-white sm:text-5xl text-balance">
+					Protect your intellectual property
+				</h2>
+				<div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:shrink-0">
+					<CTA invertColours />
+					<a href="/articles/about" className="text-sm/6 font-semibold text-white">
+						Learn more <span aria-hidden="true">→</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	)
+}
