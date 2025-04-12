ba514ff4fc05b3af2159c6c5d0d9db93d0f55699
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-12 10:19:42 +0100

    Destructure props and update styles

--- src/app/team/TeamMemberCard.tsx
+++ src/app/team/TeamMemberCard.tsx
@@ -2,20 +2,27 @@ import type { TeamMember } from '@/types'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function TeamMemberCard({ teamMember, priority }: { teamMember: TeamMember; priority: boolean }) {
+export default function TeamMemberCard({
+	teamMember: { slug, featuredImage, metaDescription, title, role, content },
+	priority,
+}: { teamMember: TeamMember; priority: boolean }) {
 	return (
-		<Link href={`/team/${teamMember.slug}`}>
-			<h2 className="text-xl font-medium">{teamMember.title}</h2>
-			<p className="mb-2">{teamMember.role}</p>
+		<Link href={`/team/${slug}`}>
 			<Image
-				src={teamMember.featuredImage}
-				alt={teamMember.metaDescription}
+				src={featuredImage}
+				alt={metaDescription}
 				width={1200}
 				height={630}
 				priority={priority}
 				// ToDo: placeholder handling
 				// placeholder="blur"
-				className="rounded-md"
+				className="rounded-md w-full mb-2"
+			/>
+			<h2 className="text-xl font-medium mb-1">{`${title}, ${role}`}</h2>
+			<p
+				className="mb-2 line-clamp-2"
+				// biome-ignore lint/security/noDangerouslySetInnerHtml:
+				dangerouslySetInnerHTML={{ __html: content }}
 			/>
 		</Link>
 	)
