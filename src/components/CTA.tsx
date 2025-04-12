b71c9ea4f09fdb90f07f29aa528ba04cb2dc816b
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-12 10:20:57 +0100

    Add inverted colours option

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -6,7 +6,7 @@ import { useProvider } from './Provider'
  * Opens the contact form modal
  * Accepts an optional onClick property so that the CTA on the mobile menu can close the mobile menu panel at the same time, otherwise scrolling is disabled and the form isn't useable
  */
-export default function CTA({ classes, onClick }: { classes?: string; onClick?: () => void }) {
+export default function CTA({ classes, onClick, invertColours }: { classes?: string; onClick?: () => void; invertColours?: boolean }) {
 	const { setContactFormVisible } = useProvider()
 
 	const handleClick = () => {
@@ -14,12 +14,16 @@ export default function CTA({ classes, onClick }: { classes?: string; onClick?: 
 		if (onClick) onClick()
 	}
 
+	const colourClasses = 'bg-green-600 hover:bg-green-500 active:bg-green-400 sm:active:bg-green-500 text-white'
+	const invertedColourClasses = 'bg-white hover:bg-zinc-100 active:bg-zinc-100 sm:active:bg-zinc-200 text-green-600'
+
 	return (
 		<button
 			type="button"
 			onClick={handleClick}
 			className={mergeClasses(
-				'bg-green-600 hover:bg-green-500 active:bg-green-400 sm:active:bg-green-500 hover:shadow-md active:shadow-md  transition-all duration-300 px-4 py-1 rounded-lg font-bold text-xl text-white',
+				'hover:shadow-md active:shadow-md  transition-all duration-300 px-4 py-1 rounded-lg font-bold text-xl ',
+				invertColours ? invertedColourClasses : colourClasses,
 				classes,
 			)}
 		>
