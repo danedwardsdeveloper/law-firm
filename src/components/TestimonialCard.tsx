3840e485946c2c329de53351038ed55b329fba84
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-12 10:20:17 +0100

    Destructure props and update styles

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -1,30 +1,33 @@
 import type { Testimonial } from '@/types/definitions/testimonial'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function TestimonialCard({
-	testimonial: { writer, photo, altText, service, company, content },
+	testimonial: {
+		writer: { name, role },
+		photo,
+		altText,
+		service,
+		company,
+		content,
+	},
 	priority,
 }: { testimonial: Testimonial; priority: boolean }) {
 	return (
-		<div className="flex flex-col md:flex-row gap-x-4">
-			<Image src={photo} alt={altText} className="w-auto max-h-72 rounded-md" priority={priority} sizes="240px" placeholder="blur" />
-			<div>
-				<div className="mb-4">
-					<h3 className="text-xl font-medium">{service}</h3>
-					<span>
-						{writer.name}
-						{', '}
-						{writer.role}
-						{', '}
-						{company && company}
-					</span>
-				</div>
-				{content.slice(0, 1).map((paragraph) => (
-					<p key={paragraph} className="mb-4">
-						{paragraph}
-					</p>
-				))}
-			</div>
+		<div>
+			<Image src={photo} alt={altText} className="w-auto max-h-72 rounded-md mb-2" priority={priority} sizes="240px" placeholder="blur" />
+			<h3 className="text-xl font-medium">{service}</h3>
+			<p className="mb-3">
+				{`${name}, role`}
+				{company && `, ${company}`}
+			</p>
+			<p className="mb-4">
+				{content.slice(0, 1)}
+				{/* Add anchor links */}
+				<Link href="/testimonials" className="text-zinc-600 link ml-2">
+					Read more
+				</Link>
+			</p>
 		</div>
 	)
 }
