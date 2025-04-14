0932765bce7c11fbc67d82b8158681696679c623
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-14 13:11:20 +0100

    Change <div> to <Link>

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 export function TestimonialCard({
 	testimonial: {
 		writer: { name, role },
+		anchor,
 		photo,
 		altText,
 		service,
@@ -14,20 +15,14 @@ export function TestimonialCard({
 	priority,
 }: { testimonial: Testimonial; priority: boolean }) {
 	return (
-		<div>
+		<Link href={`/testimonials/#${anchor}`} className="hover:opacity-80 transition-opacity duration-300">
 			<Image src={photo} alt={altText} className="w-auto max-h-72 rounded-md mb-2" priority={priority} sizes="240px" placeholder="blur" />
 			<h3 className="text-xl font-medium">{service}</h3>
 			<p className="mb-3">
-				{`${name}, role`}
+				{`${name}, ${role}`}
 				{company && `, ${company}`}
 			</p>
-			<p className="mb-4">
-				{content.slice(0, 1)}
-				{/* Add anchor links */}
-				<Link href="/testimonials" className="text-zinc-600 link ml-2">
-					Read more
-				</Link>
-			</p>
-		</div>
+			<p className="mb-4">{content.slice(0, 1)}</p>
+		</Link>
 	)
 }
