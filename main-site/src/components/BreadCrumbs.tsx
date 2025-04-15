2ea2dc20bc2d353966f523f0992f88418533bbad
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-15 17:15:59 +0100

    Formatting only

--- main-site/src/components/BreadCrumbs.tsx
+++ main-site/src/components/BreadCrumbs.tsx
@@ -1,14 +1,14 @@
-import { ChevronRightIcon } from '@heroicons/react/24/outline'
-import Link from 'next/link'
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
-export type BreadCrumbTrail = Array<{ display: string; href: string }>
+export type BreadCrumbTrail = Array<{ display: string; href: string }>;
 
 export default function BreadCrumbs({
 	trail,
 	current,
 }: {
-	trail?: BreadCrumbTrail
-	current: string
+	trail?: BreadCrumbTrail;
+	current: string;
 }) {
 	return (
 		<nav className="mb-12">
@@ -17,18 +17,24 @@ export default function BreadCrumbs({
 					<Link href="/" className="text-zinc-600 link">
 						Home
 					</Link>
-					<ChevronRightIcon className="size-5 text-zinc-600 flex-shrink-0" aria-hidden />
+					<ChevronRightIcon
+						className="size-5 text-zinc-600 flex-shrink-0"
+						aria-hidden
+					/>
 				</li>
 				{trail?.map((item) => (
 					<li key={item.href} className="flex gap-x-3 items-center">
 						<Link href={item.href} className="text-zinc-600 link">
 							{item.display}
 						</Link>
-						<ChevronRightIcon className="size-5 text-zinc-600 flex-shrink-0" aria-hidden />
+						<ChevronRightIcon
+							className="size-5 text-zinc-600 flex-shrink-0"
+							aria-hidden
+						/>
 					</li>
 				))}
 				<li className="font-semibold">{current}</li>
 			</ul>
 		</nav>
-	)
+	);
 }
